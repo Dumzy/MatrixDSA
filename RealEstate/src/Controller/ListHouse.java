a585c5d48194543c94c4fde20c17da0c295dbd94
roviudesh <roviudesh@gmail.com>
2015-03-18 18:49:22 +0530

    View and Classes

---
+++ RealEstate/src/Controller/ListHouse.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controller;
+
+/**
+ *
+ * @author Milton
+ */
+public class ListHouse implements Listable{
+
+    @Override
+    public void copy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int compareTo(int lotNumber) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
